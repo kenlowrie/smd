c841361ee14725259158fbff2d1da987091ac76e
Ken Lowrie <klowrie@gmail.com>
2020-05-27 15:47:38 -0500

    Initial pass at updating the pip install config file.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 from sys import version_info
 
-setup(name='livesmd',
+setup(name='smd',
       version='0.3.8',
-      description='Live Script Markdown',
+      description='Script Markdown',
       url='https://github.com/kenlowrie/smd',
       author='Ken Lowrie',
       author_email='ken@kenlowrie.com',
       license='Apache',
-      packages=['livesmd', 'livesmd.smd'],
+      packages=['smd', 'smd.core'],
       install_requires=['kenl380.pylib'],
       entry_points = {
-        'console_scripts': ['livesmd=livesmd.livesmd:smd_parse_file',
-                            'mklivesmd=livesmd.mklivesmd:mklivesmd',
+        'console_scripts': ['smd=smd.smd:smd_parse_file',
+                            'livesmd=smd.livesmd:livesmd',
         ],
       },
       include_package_data=True,
