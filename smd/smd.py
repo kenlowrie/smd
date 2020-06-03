21d8c2cf1f77d812708997517e9217dad3717e3b
Ken Lowrie <klowrie@gmail.com>
2020-06-03 17:09:52 -0500

    Removed lots of old code/features ...
    To hopefully make it easier to clean up the code for the new version. Removed concepts of "Shot, Div, Alias and Shotlist". I will try to reproduce with namespace elements. This allowed me to remove the peekAhead logic, and all the handling code. Always pass the args along to avoid losing context.

--- smd/smd.py
+++ smd/smd.py
@@ -129,31 +129,26 @@ class ScriptParser(StdioWrapper):
 
 
         self._css_class_prefix = Regex(r'\{:([\s]?.\w[^\}]*)\}(.*)')
-        #//TODO: Next line was attempt at allowing embedded quotes inside id="string" parameters
-        #self._special_parameter = Regex(r'\s([\w]+)\s*=\s*\"(.*?)(?<!\\)\"(?=[,|\s)])')
         self._special_parameter = Regex(r'\s([\w]+)\s*=\s*\"(.*?)(?<!\\)\"')
 
         # Dictionary of each line type that we can process
         self._regex_main = {
-            #                   NewDiv RawLine Prefix   Test Regex                                        Match Regex
-            'shot': RegexMain(True, False, True, r'^[-|\*|\+][ ]*(?![-]{2})', r'^[-|\*|\+][ ]*(?![-]{2})(.*)'),
-            'div': RegexMain(True, False, True, r'^[-@]{3}[ ]*([\w\.]+)?[ ]*', r'^[-@]{3}[ ]*([\w\.]+)?[ ]*(.*)'),
-            'header': RegexMain(True, False, True, r'^([#]{1,6})[ ]*', r'^([#]{1,6})[ ]*(.*)'),
-            'alias': RegexMain(True, True, False, r'^\[([^\]]+)\](?=([\=](.+)))', None),
-            'import': RegexMain(True, False, False, r'^[@]import[ ]+[\'|\"](.+[^\'|\"])[\'|\"]', None),
-            'embed': RegexMain(True, False, False, r'^[@]embed[ ]+[\'|\"](.+[^\'|\"])[\'|\"]', None),
-            'image': RegexMain(True, True, False, r'^(@image(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None),
-            'var': RegexMain(True, True, False, r'^(@var(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None), 
-            'set': RegexMain(True, True, False, r'^(@set(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None),
-            'code': RegexMain(True, True, False, r'^(@code(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None), 
-            'link': RegexMain(True, True, False, r'^(@link(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None), 
-            'html': RegexMain(True, True, False, r'^(@html(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None), 
-            'dump': RegexMain(True, True, False, r'^(@dump(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)', None),
-            'break': RegexMain(True, True, False, r'^[@](break|exit)\s*$', None),
-            'stop': RegexMain(True, True, False, r'^[@](stop|quit)\s*$', None),
-            'raw': RegexMain(True, False, False, r'^@(@|raw)[ ]+(.*)', None),
-            'debug': RegexMain(True, True, False, r'^(@debug(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")*)', None),
-            'shotlist': RegexMain(True, True, False, r'^[/]{3}Shotlist[/]{3}', None),
+            # //TODO: Is NewDiv still needed?
+            #                   NewDiv RawLine Prefix   Test Regex                                              Match Regex
+            'header': RegexMain(True,   False,  True,   r'^([#]{1,6})[ ]*',                                   r'^([#]{1,6})[ ]*(.*)'),
+            'import': RegexMain(True,   False,  False,  r'^[@]import[ ]+[\'|\"](.+[^\'|\"])[\'|\"]',         None),
+            'embed': RegexMain(True,    False,  False,  r'^[@]embed[ ]+[\'|\"](.+[^\'|\"])[\'|\"]',          None),
+            'var': RegexMain(True,      True,   False,  r'^(@var(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',      None), 
+            'set': RegexMain(True,      True,   False,  r'^(@set(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',      None),
+            'code': RegexMain(True,     True,   False,  r'^(@code(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',     None), 
+            'link': RegexMain(True,     True,   False,  r'^(@link(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',     None), 
+            'html': RegexMain(True,     True,   False,  r'^(@html(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',     None), 
+            'image': RegexMain(True,    True,   False,  r'^(@image(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',    None),
+            'dump': RegexMain(True,     True,   False,  r'^(@dump(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")+)',     None),
+            'break': RegexMain(True,    True,   False,  r'^[@](break|exit)\s*$',                             None),
+            'stop': RegexMain(True,     True,   False,  r'^[@](stop|quit)\s*$',                              None),
+            'raw': RegexMain(True,      False,  False,  r'^@(@|raw)[ ]+(.*)',                                None),
+            'debug': RegexMain(True,    True,   False,  r'^(@debug(\s*([\w]+)\s*=\s*\"(.*?)(?<!\\)\")*)',    None),
         }
 
     @property
@@ -202,7 +197,6 @@ class ScriptParser(StdioWrapper):
         new 'raw' line, and try again.
         """
         if self._line.original_line.strip() != self._line.current_line.strip():
-            #print('reprocess line 0<br />\n{}<br />\n{}<br />'.format(self._line.original_line,self._line.current_line))
             # Save the current css_prefix, cause we'll lose it in _setLineAttrs()
             saved_css_prefix = self._line.css_prefix
             self._setLineAttrs(self._line.current_line)
@@ -212,10 +206,9 @@ class ScriptParser(StdioWrapper):
             if saved_css_prefix:
                 self._line.css_prefix = saved_css_prefix
             self._unreadLine()
-            #print('reprocess line 1<br />\n{}<br />\n{}<br />'.format(self._line.original_line,self._line.current_line))
             return True
-
-        #print('reprocess line failing...<br />')
+        
+        #//TODO: Should we trap this failure? What does it mean?
         return False
 
     def _setLineAttrs(self, line):
@@ -234,20 +227,6 @@ class ScriptParser(StdioWrapper):
 
         # strip the class, if any, and initialize css_prefix
         self._line.css_prefix, self._line.stripped_line = self._stripClass(line.strip())
-
-        # process any markdown
-        #self._line.current_line = self._markdown(stripped_line)
-        #self._line.current_line = self._md.markdown(stripped_line)
-        
-        # here's a hack! - Causes tests to fail, but didn't look if it's an issue
-        # or a side affect of how the test script was using the tags...
-        # this seems like a more generic way to handle adding classes via
-        # markdown variable substitution, but for now it's being handled inside
-        # peekPlainText()
-        #new_css_prefix, stripped_line = self._stripClass(line.strip())
-        #if new_css_prefix:
-        #    self._line.current_line = stripped_line
-        #    self._line.css_prefix = new_css_prefix
 
     def _readNextLine(self):
         """
@@ -322,99 +301,6 @@ class ScriptParser(StdioWrapper):
         # no class found, so return '' and the line, as-is
         return '', line
 
-    def _peekNextLine(self, element="p", addLinks=False):
-        """
-        Read next line, and if indented, add it as a new <element>
-
-        If the next line has white space at the beginning, then add it to the
-        current line as a new <p> or whatever 'element' is. Then keep looking,
-        in case there are more indented lines. This allows us to have multiple
-        lines for DIVs that are section headers, and it's also used to gather
-        multiple shots when we are processing an AV DIV.
-        """
-        if (self._readNextLine() == ''):
-            return ""   # if we hit EOF, return ''
-
-        # if line was indented, it's part of the previous element
-        if self._line.was_indented:
-            # get the link anchor and insert it ahead of the shot
-            link = "" if not addLinks else self._addBookmark(self._line.current_line)
-            # return this, but call myself recursively in case there are more indented lines...
-            return self._html.formatLine("{3}<{0}{2}>{1}</{0}>\n".format(element, self._line.current_line.rstrip('\n'), self._line.css_prefix, link) + self._peekNextLine(element, addLinks))
-
-        # We read 1 too many lines, so mark the cache as dirty so we can process it next time.
-        self._unreadLine()
-        return ""
-
-    def _peekPlainText(self, element="p"):
-        """
-        Gobble up all the normal lines after one or more shot descriptions.
-
-        "Normal" lines are the voiceover (VO) or narration that goes along with
-        each shot. Usually, there are more "VO lines" than shots, so we want to
-        read them all, and place them within the same DIV section. Each of these
-        narrative lines can have it's own class override...
-        """
-        if (self._readNextLine() == ''):
-            return ""   # if we hit EOF return ''
-
-        # This internal API detects if the next line is some type of BREAK.
-        # e.g. a section, new shot, heading or link...
-        def isNewSection(lineObj):
-            """Determines if the current line is the start of a new section.
-
-            Arguments:
-                lineObj -- a Line() instance
-
-            Returns:
-                True -- if the current line should start a new section
-                False -- if the current line is part of the current section
-            """
-            # Look to see if the current line in the cache is the start of a new section
-
-            for id in self._regex_main:
-                obj = self._regex(id)
-                # Make sure that this regex obj starts_new_div.
-                if(not obj.starts_new_div):
-                    continue
-                # if this RE allows a class prefix, use the line with the
-                # prefix stripped off, otherwise, use the original line.
-                if match(obj.test_str.regex, lineObj.current_line if obj.allows_class_prefix else lineObj.original_line):
-                    return True
-
-            return False
-
-        # if it's not a new section, keep it and peek at next line
-        if not isNewSection(self._line):
-            # add this line to the current DIV, and keep reading until we hit some
-            # type of break element...
-            if not self._line.css_prefix:
-                # if there isn't a css_prefix inline, check to see if one was added
-                # via markdown processing...
-                new_prefix, new_line = self._stripClass(self._line.current_line)
-                if new_prefix:
-                    # if the markdown added a css prefix, then adjust the _line object
-                    # so the css_prefix and current_line are updated before we format
-                    # the element.
-                    self._line.css_prefix = new_prefix
-                    self._line.current_line = new_line
-
-            return self._html.formatLine("<{0}{2}>{1}</{0}>\n".format(element, self._line.current_line, self._line.css_prefix) + self._peekPlainText(element))
-
-        # read one too many lines, so cache the current line for the next time around...
-        self._unreadLine()
-        return ""
-
-    def _printInExtrasDiv(self, str):
-        """
-        Print the string argument inside an HTML DIV Element with class="extras"
-
-        This allows orphaned elements to be kept out of the shot/narrative
-        sections, where floats are active.
-        """
-        self.oprint(self._html.formatLine("<div class=\"extras\">", 1))
-        self.oprint(self._html.formatLine(str, -1))
-        self.oprint(self._html.formatLine("</div>"))
 
     def parse(self):
         """Parse a Script File in text format and emit HTML code."""
@@ -436,43 +322,11 @@ class ScriptParser(StdioWrapper):
             line = line_obj.current_line if not regex_obj.uses_raw_line else line_obj.original_line
             return match(regex_obj.match_regex(), line)
 
-        def handle_shot(m, lineObj):
-            """Handle a shot parse line"""
-            if(m is not None):
-                li = "" if len(m.groups()) < 1 else "%s" % m.group(1)
-                divstr = self._html.formatLine("<div class=\"av\">\n", 1)
-                divstr += self._addBookmark(li)
-                divstr += self._html.formatLine("<ul>\n", 1)
-                divstr += self._html.formatLine("<li{1}>{0}</li>\n".format(li, lineObj.css_prefix))
-                divstr += self._peekNextLine("li", True)
-                divstr += self._html.formatLine("</ul>\n", -1, False)
-                divstr += self._peekPlainText()
-                divstr += self._html.formatLine("</div>", -1, False)
-                self.oprint(divstr)
-            else:
-                self.oprint(lineObj.current_line)
-
-        def handle_div(m, lineObj):
-            """handle a DIV parse line"""
-            if(m is not None):
-                divClas = "" if not lineObj.css_prefix else lineObj.css_prefix
-                divPcls = "" if len(m.groups()) < 1 or m.group(1) == "." else " class=\"%s\"" % m.group(1)
-                divText = "" if len(m.groups()) < 2 else "%s\n" % m.group(2)
-                divstr = self._html.formatLine("<div%s>\n" % divClas, 1)
-                divstr += self._html.formatLine("<p%s>%s</p>\n" % (divPcls, divText.rstrip('\n')))
-                divstr += self._peekNextLine()
-                divstr += self._html.formatLine("</div>", -1, False)
-
-                self.oprint(divstr)
-
-            else:
-                self.oprint(lineObj.current_line)
-
         def handle_header(m, lineObj):
             """Handle a header parse line"""
             if(m is not None and len(m.groups()) > 1):
                 hnum = len(m.group(1))
-                self._printInExtrasDiv("<h{0}{2}>{1}</h{0}>".format(hnum, m.group(2).strip(), lineObj.css_prefix))
+                self.oprint("<h{0}{2}>{1}</h{0}>".format(hnum, m.group(2).strip(), lineObj.css_prefix))
             else:
                 self.oprint(lineObj.current_line)
 
@@ -549,37 +403,6 @@ class ScriptParser(StdioWrapper):
                         
             else:
                 self.oprint(lineObj.current_line)
-
-
-        # //TODO: Should this be a namespace?
-        def handle_shotlist(m, lineObj):
-            """Handle a shotlist parse line."""
-            self.oprint(self._html.formatLine("<div class=\"shotlist\">", 1))
-            self.oprint(self._html.formatLine("<hr />"))
-            self.oprint(self._html.formatLine("<p>Shotlist</p>"))
-            self.oprint(self._html.formatLine("<code>", 1))
-            shotnum = 1
-            for shot in self._shotListQ.getBookmarkList():
-                self.oprint(self._html.formatLine("<div class=\"shot\">", 1))
-                self.oprint(self._html.formatLine("<p>{1}&#160;"
-                                                  "<a class=\"shotlist-backref\""
-                                                     " href=\"#{0}\" rel=\"tag\""
-                                                     " title=\"Jump back to shot {2} in the script\""
-                                                  ">&#8617;</a></p>".format(shot[0], shot[1], shotnum), -1))
-                self.oprint(self._html.formatLine("</div>"))
-                shotnum += 1
-
-            self.oprint(self._html.formatLine("</code>", -1, False))
-            self.oprint(self._html.formatLine("</div>", -1, False))
-
-        def handle_alias(m, lineObj):
-            """Handle the alias parse line type"""
-            if(m is not None and len(m.groups()) == 3):
-                #self._variables.addVar(m.group(1), self._markdown(m.group(3)))
-                #TODO: Should m.group(3) be self._md.markdown(m.group(3))?
-                self._ns.addVariable(self._md.markdown(m.group(3)), name=m.group(1), ns="basic")
-            else:
-                self.oprint(lineObj.original_line)
 
         def handle_dump(m, lineObj):
             """Handle the dump parse line type."""
@@ -680,12 +503,8 @@ class ScriptParser(StdioWrapper):
             ('break', handle_break),
             ('embed', handle_embed),
             ('stop', handle_stop),
-            ('shotlist', handle_shotlist),
-            ('alias', handle_alias),
             ('debug', handle_debug),
             ('dump', handle_dump),
-            ('shot', handle_shot),
-            ('div', handle_div),
             ('header', handle_header),
             ('raw', handle_raw),
             ('import', handle_import),
@@ -708,8 +527,9 @@ class ScriptParser(StdioWrapper):
 
                 # if no parse type was matched, then just write it out
                 if not matched and self._line.current_line.rstrip() and not self._reprocessLine():
-                    divstr = self._line.current_line
-                    self.oprint(divstr)
+                    # If the line has a class prefix, then write it out inside a span
+                    span = f'<span{self._line.css_prefix}>{{}}</span>' if self._line.css_prefix else '{}'
+                    self.oprint(span.format(self._line.current_line))
 
             #//TODO: Not sure I like how the _tls data is being used. I need an object/class for it...
             from .core.config import ConfigFile
@@ -811,7 +631,7 @@ def smd_parse_file(args=None):
                             epilog=".")
     parser.add_argument('-f', '--filename', help='the file that you want to parse. Default is stdin if not specified')
 
-    args = smd_add_std_cmd_line_parms(parser, sysDefaults)
+    args = smd_add_std_cmd_line_parms(parser, sysDefaults, args)
 
     return ScriptParser(sysDefaults).open_and_parse(args.filename)
 
