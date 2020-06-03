da8f48450f8a8e1a90eab02e717e6772cc0ee21c
Ken Lowrie <klowrie@gmail.com>
2020-06-03 16:56:47 -0500

    Change cwd so things will load w/o path

--- tests/debug.py
+++ tests/debug.py
@@ -7,11 +7,17 @@ bin_path, whocares = split(dirname(realpath('__file__')))
 lib_path = join(abspath(bin_path),'smd')
 path.insert(0, lib_path)
 """
+from pathlib import Path
+from os import chdir
+
+parent = Path(__file__).parent
+chdir(Path().joinpath(parent,'sos'))
 
 import smd
 
 #from smd import smd_parse_file
 
-smd.smd.smd_parse_file(['-f', 'test.md'])
+#smd.smd.smd_parse_file(['-f', 'clean.md', '-nd', '-ndb'])
+smd.smd.smd_parse_file(['-nd', '-ndb'])
 
 #avscript.avscript_md.av_parse_file(['-f', '/Users/ken/Dropbox/clp/projects/2018/buddyvargas/video/forgiveme/forgiveme-musicvideo.md'])
