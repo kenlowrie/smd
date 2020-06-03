1f9864494c1c336c3e5a07d99662dea4ecfc48db
Ken Lowrie <klowrie@gmail.com>
2020-06-03 12:14:08 -0500

    Updates to allow source debugging in VS Code

--- tests/debug.py
+++ tests/debug.py
@@ -4,14 +4,14 @@
 from sys import path
 from os.path import dirname, abspath, realpath, split, join
 bin_path, whocares = split(dirname(realpath('__file__')))
-lib_path = join(abspath(bin_path),'avscript')
+lib_path = join(abspath(bin_path),'smd')
 path.insert(0, lib_path)
 """
 
-import avscript
+import smd
 
-#from avscript_md import av_parse_file
+#from smd import smd_parse_file
 
-avscript.avscript_md.av_parse_file(['-f', 'test.md'])
+smd.smd.smd_parse_file(['-f', 'test.md'])
 
 #avscript.avscript_md.av_parse_file(['-f', '/Users/ken/Dropbox/clp/projects/2018/buddyvargas/video/forgiveme/forgiveme-musicvideo.md'])
