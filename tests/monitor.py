fb57f30a900f46b9eead6f339b889e00c5113cb9
Ken Lowrie <klowrie@gmail.com>
2020-06-07 16:18:29 -0500

    Refactored the code.
    Much simpler and cleaner now. Will need to modify for use with different logger, but I think the code is ready for integration into ismd.py...

--- tests/monitor.py
+++ tests/monitor.py
@@ -35,19 +35,10 @@ class watchlist(object):
     
     def release(self):
         self.lock.release()
+    
+    def exists(self, name):
+        return True if name in self.files else False
 
-    """
-    def duplicateList(self):
-        newlist = []
-        self.acquire()
-        try:
-            msg(f"watchlist: duplicating list...")
-            newlist = [i for i in self.files]
-        finally:
-            self.release()
-        
-        return newlist
-    """
     def addItem(self, filename):
         self.acquire()
         try:
@@ -55,16 +46,7 @@ class watchlist(object):
             self.files.append(filename)
         finally:
             self.release()
-    """
-    def removeItem(self, filename):
-        self.acquire()
-        try:
-            if filename in self.files:
-                msg(f"watchlist: removing [{filename}]")
-                self.files.remove(filename)
-        finally:
-            self.release()
-    """    
+
     def dump(self):
         self.acquire()
         try:
@@ -72,25 +54,8 @@ class watchlist(object):
                 msg(f"\t{i}")
         finally:
             self.release()
-
 """
-class simplelist(watchlist):
-    def __init__(self, filename):
-        # don't call the constructor, we don't need a lock. Just overridding
-        # acquire and release, rest of baseclass methods are fine for this class
-        self.files = []
-        self.addItem(filename)
-    
-    def acquire(self):
-        pass
-    
-    def release(self):
-        pass
-
-"""
-
 from watchdog.events import FileSystemEventHandler
-#from watchdog.observers.api import EventQueue
 
 class watchdir(FileSystemEventHandler):
     def __init__(self, observer, fullpath):
@@ -101,7 +66,6 @@ class watchdir(FileSystemEventHandler):
         self.watcher = None                 # So we don't unschedule if we never scheduled
         self.files = watchlist(fullpath.name)
         self.q = SimpleQueue()
-        #self.thread = Thread(target=self.monitor, name=f"{id(self)}")
     
     def addItem(self, filespec):
         self.files.addItem(filespec.name)
@@ -126,70 +90,73 @@ class watchdir(FileSystemEventHandler):
 
     def dump(self):
         self.files.dump()
-
+"""
 from watchdog.events import FileSystemEventHandler
 
-class watcher(object):
+class watcher(FileSystemEventHandler):
     def __init__(self):
+        super(watcher, self).__init__()  # Initialize the base class(es)
+
         self.directories = {}
+        self.watchers = []
+        self.q = SimpleQueue()
+
         from watchdog.observers import Observer
         self.observer = Observer()
         msg(f"Starting Observer()")
         self.observer.start()
 
+    def _addWatchItem(self,item):
+        filespec = Path(item).resolve()
+
+        if filespec.parent not in self.directories:
+            msg(f"Creating watchlist() for [{filespec}]")
+            self.directories[filespec.parent] = watchlist(filespec.name)
+        else:
+            self.directories[filespec.parent].addItem(filespec.name)
+
+    def _start(self, directory):
+        msg(f"Scheduling watcher for [{directory}]")
+        self.watchers.append(self.observer.schedule(self, str(directory), recursive = False))
+
+    def _stop(self, watcher):
+        if watcher in self.watchers:
+            #//TODO: This won't work as-is. If needed, should make a small class to hold the
+            # watcher stuff: directory, the watcher instance, etc.
+            msg(f"UnScheduling watcher for [{directory}]")
+            self.observer.unschedule(watcher)
+
+    def on_modified(self, event):
+        if event.is_directory: return None
+
+        spec = Path(event.src_path).resolve()
+        #path = spec.parent
+        #name = spec.name
+        if spec.parent in self.directories:
+            if self.directories[spec.parent].exists(spec.name):
+                msg(f"File {event.src_path} was modified. Adding to queue...")
+                self.q.put(event.src_path)
+            else:
+                msg(f"File {event.src_path} was modified, but I don't care about it...")
+        else:
+            msg(f"Got notification of change to {spec.name} in directory {spec.parent} --> {event.src_path}")
+
     def begin(self, filelist):
         # first, create all the watchdir() for each unique directory
         for item in filelist:
-            self.addWatchItem(item)
+            self._addWatchItem(item)
 
         # now, schedule an event handler(watchdog.watch) for each folder
         for dir in self.directories:
-            self.directories[dir].start()
+            self._start(dir)
     
-
-    def refresh(self, filelist):
+    def reset(self, filelist):
         # get list of directories being monitored
-        msg("refreshing list of directories")
-        """
-        tmplist = {}
-        for item in filelist:
-            spec = Path(item).resolve()
-            if spec.parent not in tmplist:
-                msg(f"Creating watchlist() for [{spec}]")
-                tmplist[spec.parent] = simplelist(spec.name)
-            else:
-                tmplist[spec.parent].addItem(spec.name)
-        msg(f"updated file list:")
-        for dir in tmplist:
-            msg(f"updated files in dir: {dir}")
-            tmplist[dir].dump()
-        """
-        msg("shutting down directory observers...")
+        msg("reset: shutting down directory observers...")
         self.observer.unschedule_all()
         self.directories = {}
+        self.watchers = []
         self.begin(filelist)
-        """
-        for dir in self.directories:
-            if dir not in tmplist:
-                msg(f"directory {dir} no longer being monitored, removing ...")
-                self.directories[dir].stop()
-                del self.directories[dir]
-            else:
-                try:
-                    msg(f"looking for files no longer being monitored...")
-                    watchlist = self.directories[dir].files.duplicateList()
-                    for file in watchlist:
-                        if file not in tmplist[dir].files:
-                            self.directories[dir].files.removeItem(file)
-
-                    msg(f"looking for files that have been added to existing folder...")
-                    watchlist = self.directories[dir].files.duplicateList()
-                    for file in tmplist[dir].files:
-                        if file not in watchlist:
-                            self.directories[dir].files.addItem(file)
-                finally:
-                    pass
-        """
 
     def end(self):
         self.observer.unschedule_all()
@@ -197,18 +164,9 @@ class watcher(object):
         msg("\nObserver Stopped")
         self.observer.join()
 
-    def addWatchItem(self,item):
-        filespec = Path(item).resolve()
-
-        if filespec.parent not in self.directories:
-            msg(f"Creating watchlist() for [{filespec}]")
-            self.directories[filespec.parent] = watchdir(self.observer, filespec)
-        else:
-            self.directories[filespec.parent].addItem(filespec)
-
     def look(self):
-        for i in self.directories:
-            self.directories[i].look()
+        if self.q.qsize():
+            msg(f"Modified: {self.q.get()}")
 
     def dump(self):
         for i in self.directories:
@@ -216,19 +174,18 @@ class watcher(object):
             self.directories[i].dump()
 
 w = watcher()    
-#for item in filelist:
-#    w.addWatchItem(item)
+
 msg(f'{"*"*100}')
 w.begin(filelist)
 w.dump()
 msg(f'{"*"*100}')
-w.refresh(filelist[:-1])
+w.reset(filelist[:-1])
 w.dump()
 msg(f'{"*"*100}')
-w.refresh(filelist[:-2])
+w.reset(filelist[:-2])
 w.dump()
 msg(f'{"*"*100}')
-w.refresh(filelist[1:])
+w.reset(filelist[1:])
 w.dump()
 msg(f'{"*"*100}')
 
